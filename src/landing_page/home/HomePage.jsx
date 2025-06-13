2cf2f05bb1c273dc15547868cfc7aa01145dde57
VedantPatil-99 <vedantpatil9977@gmail.com>
2025-06-13 14:38:58 +0530

    feat: replace Header with Navbar in HomePage and update main.jsx to render HomePage Embed Font Awesome Kit through js

--- src/landing_page/home/HomePage.jsx
+++ src/landing_page/home/HomePage.jsx
@@ -5,13 +5,13 @@ import Pricing from "./Pricing";
 import Education from "./Education";
 import Stats from "./Stats";
 import OpenAccount from "../OpenAccount";
-import Header from "../Header";
+import Navbar from "../Navbar";
 import Footer from "../Footer";
 
 const HomePage = () => {
 	return (
 		<>
-			<Header />
+			<Navbar />
 
 			<HeroSection />
 			<Awards />
