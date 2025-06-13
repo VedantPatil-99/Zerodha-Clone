95ee7d8bb515759041c7b1d1165166c636e5e4b3
VedantPatil-99 <vedantpatil9977@gmail.com>
2025-06-13 12:57:20 +0530

    feat: add Brokerage and HeroSection components to PricingPage

---
+++ src/landing_page/pricing/PricingPage.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import HeroSection from "./HeroSection";
+import Brokerage from "./Brokerage";
+
+const PricingPage = () => {
+	return (
+		<>
+			<HeroSection />
+			<Brokerage />
+		</>
+	);
+};
+
+export default PricingPage;
