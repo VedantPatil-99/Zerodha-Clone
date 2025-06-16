8a0cba204762a3cccbf44240aead507441c8065c
VedantPatil-99 <vedantpatil9977@gmail.com>
2025-06-16 17:27:00 +0530

    refactor: Update Education component structure and replace WhyUsFrag with direct content rendering

--- src/landing_page/home/Education.jsx
+++ src/landing_page/home/Education.jsx
@@ -1,19 +1,34 @@
 import eduImg from "@assets/images/education.svg";
 import helpers from "@/helpers.json";
-import WhyUsFrag from "@pages/home/WhyUsFrag";
+import ArrowLink from "@components/common/ArrowLink";
 const Education = () => {
 	return (
 		<>
-			<section className="section-spacing holder">
-				<div className="flex flex-col md:flex-row gap-8 items-center justify-between">
-					<div className="mb-8 md:mb-0">
-						<img src={eduImg} alt="Education" className="w-full" />
-					</div>
-					<div className="">
-						<h2 className="header-1">Free and open market education</h2>
-						{helpers.eduData.map((item, idx) => (
-							<WhyUsFrag key={idx} description={item.description} />
-						))}
+			<section className="section-spacing">
+				<div className="holder">
+					<div className="flex flex-col md:flex-row gap-32 items-center justify-between">
+						<div className="">
+							<img
+								src={eduImg}
+								alt="Education"
+								className="mb-8 md:mb-0 w-3xl"
+							/>
+						</div>
+						<div className="">
+							<h2 className="header-1">Free and open market education</h2>
+							{helpers.eduData.map((item, idx) => (
+								<>
+									<p
+										idx={idx}
+										className="text-lg text-slate-500 my-4 leading-8">
+										{item.description}
+									</p>
+									<ArrowLink href={idx ? "#" : "#"} className="mb-8">
+										{idx ? "TradingQ&A" : "Varsity"}
+									</ArrowLink>
+								</>
+							))}
+						</div>
 					</div>
 				</div>
 			</section>
