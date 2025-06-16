336d760c7a91f731e19940f57f4b0528e68f02dc
VedantPatil-99 <vedantpatil9977@gmail.com>
2025-06-17 02:12:43 +0530

    fix: Add className to anchor elements in LinkStripFooter for improved styling

--- src/components/layout/LinkStripFooter.jsx
+++ src/components/layout/LinkStripFooter.jsx
@@ -7,7 +7,11 @@ const LinkStripFooter = () => {
 				{helpers.footerStrip.map((link, linkIdx) => {
 					return (
 						<li key={linkIdx}>
-							<a href={link.url}>{link.name}</a>
+							<a
+								href={link.url}
+								className="text-zinc-500 hover:text-blue-500 opacity-70 font-medium">
+								{link.name}
+							</a>
 						</li>
 					);
 				})}
