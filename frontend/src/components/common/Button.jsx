e7dc748fedb92bee983a41c33bbb7bb9a4544c2d
VedantPatil-99 <vedantpatil9977@gmail.com>
2025-06-19 00:32:30 +0530

    fix: Update hover background color in Button component for consistency

--- frontend/src/components/common/Button.jsx
+++ frontend/src/components/common/Button.jsx
@@ -2,7 +2,7 @@ export default function Button({ children, type = "button", className = "" }) {
 	return (
 		<button
 			type={type}
-			className={`bg-sky-600 text-white text-xl font-stretch-condensed font-medium p-2 w-50 rounded-sm hover:bg-black ${className}`}>
+			className={`bg-sky-600 text-white text-xl font-stretch-condensed font-medium p-2 w-50 rounded-sm hover:bg-neutral-800 ${className}`}>
 			{children}
 		</button>
 	);
