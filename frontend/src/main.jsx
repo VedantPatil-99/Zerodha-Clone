a7649a2003125ec8643def57aa6422fcb72d03ed
VedantPatil-99 <vedantpatil9977@gmail.com>
2025-06-18 21:22:37 +0530

    feat: Implement routing with react-router-dom in main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,24 @@
-import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./index.css";
+
+import AboutPage from "./landing_page/about/AboutPage";
 import HomePage from "./landing_page/home/HomePage";
+import PricingPage from "./landing_page/pricing/PricingPage";
+import ProductsPage from "./landing_page/products/ProductsPage";
+import SignupPage from "./landing_page/signup/SignupPage";
+import SupportPage from "./landing_page/support/SupportPage";
 
 createRoot(document.getElementById("root")).render(
-	<StrictMode>
-		<HomePage />
-	</StrictMode>,
+	<BrowserRouter>
+		<Routes>
+			<Route path="/" element={<HomePage />} />
+			<Route path="/signup" element={<SignupPage />} />
+			<Route path="/about" element={<AboutPage />} />
+			<Route path="/products" element={<ProductsPage />} />
+			<Route path="/pricing" element={<PricingPage />} />
+			<Route path="/support" element={<SupportPage />} />
+			{/* <Routes path="*" element={ } /> */}
+		</Routes>
+	</BrowserRouter>,
 );
