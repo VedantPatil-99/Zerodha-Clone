c6168a34751ee10deed4d2a703de563b0b075723
VedantPatil-99 <vedantpatil9977@gmail.com>
2025-06-22 16:23:39 +0530

    refactor: Simplify structure of Education component for improved readability

--- frontend/src/landing_page/home/Education.jsx
+++ frontend/src/landing_page/home/Education.jsx
@@ -7,28 +7,22 @@ const Education = () => {
 	return (
 		<>
 			<section className="section-spacing">
-				<div className="holder">
-					<div className="flex flex-col gap-4 md:flex-row md:gap-32 items-center md:justify-between">
-						<div className="">
-							<img
-								src={eduImg}
-								alt="Education"
-								className="mb-8 md:mb-0 w-3xl"
-							/>
-						</div>
-						<div className="">
-							<h2 className="header-1">Free and open market education</h2>
-							{helpers.eduData.map((item, idx) => (
-								<React.Fragment key={idx}>
-									<p className="text-lg text-slate-500 my-4 leading-8">
-										{item.description}
-									</p>
-									<ArrowLink href={idx ? "#" : "#"} className="mb-8">
-										{idx ? "TradingQ&A" : "Varsity"}
-									</ArrowLink>
-								</React.Fragment>
-							))}
-						</div>
+				<div className="holder flex flex-col gap-4 md:flex-row md:gap-32 items-center md:justify-between">
+					<div>
+						<img src={eduImg} alt="Education" className="mb-8 md:mb-0 w-3xl" />
+					</div>
+					<div>
+						<h2 className="header-1">Free and open market education</h2>
+						{helpers.eduData.map((item, idx) => (
+							<React.Fragment key={idx}>
+								<p className="text-lg text-slate-500 my-4 leading-8">
+									{item.description}
+								</p>
+								<ArrowLink href={idx ? "#" : "#"} className="mb-8">
+									{idx ? "TradingQ&A" : "Varsity"}
+								</ArrowLink>
+							</React.Fragment>
+						))}
 					</div>
 				</div>
 			</section>
