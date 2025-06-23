0a2d0ef77ff2a39ccbb7db211dec9f1ac991fcd6
VedantPatil-99 <vedantpatil9977@gmail.com>
2025-06-24 00:12:51 +0530

    feat: Refactor ProductsPage to dynamically render ImgLeftBlock and ImgRightBlock based on product data

--- frontend/src/landing_page/products/ProductsPage.jsx
+++ frontend/src/landing_page/products/ProductsPage.jsx
@@ -3,18 +3,18 @@ import HeroSection from "./HeroSection";
 import ImgLeftBlock from "./ImgLeftBlock";
 import ImgRightBlock from "./ImgRightBlock";
 import Universe from "./Universe";
+import productData from "@/productData";
 
 const ProductsPage = () => {
 	return (
 		<>
 			<HeroSection />
-
-			<ImgLeftBlock />
-			<ImgRightBlock />
-			<ImgLeftBlock />
-			<ImgRightBlock />
-			<ImgLeftBlock />
-
+			{productData.map((item) => {
+				if (item.layout === "left") {
+					return <ImgLeftBlock key={item.id} data={item} />;
+				}
+				return <ImgRightBlock key={item.id} data={item} />;
+			})}
 			<Universe />
 		</>
 	);
