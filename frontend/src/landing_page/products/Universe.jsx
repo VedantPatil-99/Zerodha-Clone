1615a51a3265c52d23c7c3043ff79c590d0ea713
VedantPatil-99 <vedantpatil9977@gmail.com>
2025-06-24 00:30:27 +0530

    feat: Link to Zerodha.tech blog

--- frontend/src/landing_page/products/Universe.jsx
+++ frontend/src/landing_page/products/Universe.jsx
@@ -1,7 +1,21 @@
 import React from "react";
 
 const Universe = () => {
-	return <div>Universe</div>;
+	return (
+		<>
+			<div className="section-spacing text-center">
+				<p className="text-xl pt-2 5 pb-4 text-zinc-600">
+					Want to know more about our technology stack? Check out the{" "}
+					<a
+						href="https://zerodha.tech/"
+						className="font-medium text-blue-700 hover:text-neutral-800 opacity-75">
+						Zerodha.tech
+					</a>{" "}
+					blog.
+				</p>
+			</div>
+		</>
+	);
 };
 
 export default Universe;
