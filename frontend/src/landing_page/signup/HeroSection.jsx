430840518618a6c2f98fd93e5a04d9edb3dcb5a9
VedantPatil-99 <vedantpatil9977@gmail.com>
2025-06-26 23:45:45 +0530

    feat: Add HeroSection layout

--- frontend/src/landing_page/signup/HeroSection.jsx
+++ frontend/src/landing_page/signup/HeroSection.jsx
@@ -1,7 +1,37 @@
-import React from "react";
+import account_open from "@assets/images/account_open.svg";
+import india_flag from "@assets/images/india-flag.svg";
+import Button from "@components/common/Button";
 
 const HeroSection = () => {
-	return <div>HeroSection</div>;
+	return (
+		<>
+			<section className="pt-5 px-10">
+				<div className="flex flex-col">
+					<img src={account_open} alt="Open Account" className="pb-7" />
+					<h1 className="block md:hidden font-medium text-4xl">
+						Open a free demat & trading account online
+					</h1>
+					<p>Signup Now</p>
+					<form action="#" method="post">
+						<div className="flex">
+							<span>
+								<img src={india_flag} alt="India-flag" className="inline" /> +91
+							</span>
+
+							<input
+								type="number"
+								name="mobile_num"
+								id="mobile_num"
+								placeholder="Enter your mobile number"
+							/>
+						</div>
+					</form>
+					<Button>Get OTP</Button>
+					<p>By proceeding, you agree to the Zerodha terms & privacy policy</p>
+				</div>
+			</section>
+		</>
+	);
 };
 
 export default HeroSection;
