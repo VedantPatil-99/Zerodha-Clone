2a9d1944fd54b325f9d74aa85f2a9b9a6071c81e
VedantPatil-99 <vedantpatil9977@gmail.com>
2025-06-29 19:38:57 +0530

    feat: Add landing title for wider screens and a button

--- frontend/src/landing_page/signup/HeroSection.jsx
+++ frontend/src/landing_page/signup/HeroSection.jsx
@@ -7,45 +7,62 @@ const HeroSection = () => {
 	return (
 		<>
 			<section className="pt-5 px-10">
-				<div className="flex flex-col text-zinc-700">
-					<img src={account_open} alt="Open Account" className="pb-7" />
+				<div className="hidden md:block text-center section-spacing-reverse mb-12">
+					<h1 className="text-[44px] font-medium text-zinc-700 mb-5 leading-14">
+						Open a free demat and trading account online
+					</h1>
+					<p class="acop-subheading text-center text-zinc-500 text-xl mt-2.5 mb-4">
+						Start investing brokerage free and join a community of{" "}
+						<span class="client-count">1.6+ crore</span> investors and traders
+					</p>
+				</div>
+				<div className="flex flex-col md:flex-row text-zinc-700">
+					<img
+						src={account_open}
+						alt="Open Account"
+						className="pb-7 max-w-md mx-auto"
+					/>
 					<h1 className="block md:hidden font-medium text-2xl mb-1">
 						Open a free demat & trading account online
 					</h1>
-					<p className="text-lg font-medium pt-2.5">Signup Now</p>
-					<form action="#" method="post" className="mt-5">
-						<div className="grid grid-cols-12 mb-7 border border-zinc-400 rounded-xs focus-within:border-zinc-500">
-							<div className="p-3 border-e border-e-zinc-300 rounded-xs w-full col-span-3 flex justify-between items-center">
-								<img
-									src={india_flag}
-									alt="India-flag"
-									className="inline me-1"
+					<div>
+						<p className="text-lg font-medium pt-2.5">Signup Now</p>
+						<form action="#" method="post" className="mt-5">
+							<div className="grid grid-cols-12 mb-7 border border-zinc-400 rounded-xs focus-within:border-zinc-500">
+								<div className="p-3 border-e border-e-zinc-300 rounded-xs w-full col-span-3 flex justify-between items-center">
+									<img
+										src={india_flag}
+										alt="India-flag"
+										className="inline me-1"
+									/>
+									<span className="block">+91</span>
+								</div>
+
+								<input
+									type="number"
+									name="mobile_num"
+									id="mobile_num"
+									placeholder="Enter your mobile number"
+									required
+									className="p-3 w-full col-span-9 border-none outline-none bg-transparent placeholder:text-zinc-400 placeholder:font-[480] placeholder:opacity-80"
 								/>
-								<span className="block">+91</span>
 							</div>
-
-							<input
-								type="number"
-								name="mobile_num"
-								id="mobile_num"
-								placeholder="Enter your mobile number"
-								required
-								className="p-3 w-full col-span-9 border-none outline-none bg-transparent placeholder:text-zinc-400 placeholder:font-[480] placeholder:opacity-80"
-							/>
-						</div>
-					</form>
-
-					<p className="my-3 text-xs font-medium text-zinc-400">
-						By proceeding, you agree to the Zerodha{" "}
-						<Link to={"/terms"} className="text-sky-700 opacity-75">
-							{" "}
-							terms
-						</Link>{" "}
-						&{" "}
-						<Link to={"/privacy-policy"} className="text-sky-700 opacity-75">
-							privacy policy
-						</Link>
-					</p>
+							<div className="mt-7">
+								<Button className="w-full mb-5">Get OTP</Button>
+							</div>
+						</form>
+						<p className="my-3 text-xs font-medium text-zinc-400">
+							By proceeding, you agree to the Zerodha{" "}
+							<Link to={"/terms"} className="text-sky-700 opacity-75">
+								{" "}
+								terms
+							</Link>{" "}
+							&{" "}
+							<Link to={"/privacy-policy"} className="text-sky-700 opacity-75">
+								privacy policy
+							</Link>
+						</p>
+					</div>
 				</div>
 			</section>
 		</>
