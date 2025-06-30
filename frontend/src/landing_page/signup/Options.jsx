1e81abfa107692bc4f21ff2541a4101bbaf5b350
VedantPatil-99 <vedantpatil9977@gmail.com>
2025-06-30 23:59:22 +0530

    feat: Implement Options component to display investment options with Zerodha demat account

--- frontend/src/landing_page/signup/Options.jsx
+++ frontend/src/landing_page/signup/Options.jsx
@@ -1,7 +1,18 @@
-import React from "react";
+import InvestOpts from "./InvestOpts";
 
 const Options = () => {
-	return <div>Options</div>;
+	return (
+		<>
+			<section className="section-spacing-reverse text-xl font-medium text-zinc-700 mt-10">
+				<h2 className="mb-12 text-center">
+					Investment options with Zerodha demat account
+				</h2>
+				<div className="grid md:grid-cols-2 py-4 gap-10">
+					<InvestOpts />
+				</div>
+			</section>
+		</>
+	);
 };
 
 export default Options;
