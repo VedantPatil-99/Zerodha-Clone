d94d61a4fc93384567107e97a58adea1c74ac884
VedantPatil-99 <vedantpatil9977@gmail.com>
2025-07-04 00:10:37 +0530

    fix: Update layout and styling of Steps component for better visual consistency

--- frontend/src/landing_page/signup/Steps.jsx
+++ frontend/src/landing_page/signup/Steps.jsx
@@ -11,18 +11,24 @@ const Steps = () => {
 					<div className="">
 						<img src={steps_acop} alt="Steps to open a demat account" />
 					</div>
-					<div className="text-[15px]">
-						<div className="flex flex-row mt-8 justify-start items-center h-12 border-bottom">
-							<span className="border rounded-full size-6 text-lg text-center">
+					<div className="text-[15px] mt-8">
+						<div className="flex flex-row justify-start items-center h-14 border-bottom">
+							<span className="block border border-zinc-300 rounded-full size-8 text-lg text-center">
 								01
 							</span>
 							<p className="m-4">Enter the requested details</p>
 						</div>
-						<div className="flex flex-row">
-							<span>02</span> <p>Complete e-sign & verification</p>
+						<div className="flex flex-row justify-start items-center h-14 border-bottom">
+							<span className="block border border-zinc-300 rounded-full size-8 text-lg text-center">
+								02
+							</span>
+							<p className="m-4">Complete e-sign & verification</p>
 						</div>
-						<div className="flex flex-row">
-							<span>03</span> <p>Start investing!</p>
+						<div className="flex flex-row justify-start items-center h-14 border-bottom">
+							<span className="block border border-zinc-300 rounded-full size-8 text-lg text-center">
+								03
+							</span>
+							<p className="m-4">Start investing!</p>
 						</div>
 					</div>
 				</div>
