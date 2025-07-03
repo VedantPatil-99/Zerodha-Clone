07345bb0c85c67106991b68fdab24c66b13426c3
VedantPatil-99 <vedantpatil9977@gmail.com>
2025-07-04 00:30:10 +0530

    feat: Update Steps component layout and styling for improved user experience

--- frontend/src/landing_page/signup/Steps.jsx
+++ frontend/src/landing_page/signup/Steps.jsx
@@ -3,32 +3,32 @@ import steps_acop from "@assets/images/steps-acop.svg";
 const Steps = () => {
 	return (
 		<>
-			<section className="section-spacing-reverse bg-zinc-50 mt-4 font-medium text-zinc-600">
-				<h2 className="text-xl mb-12 text-center  ">
+			<section className="signup-spacing bg-gray-50 md:mt-20 font-medium text-zinc-600">
+				<h2 className="text-xl md:text-4xl mb-12 mt-4 text-center  ">
 					Steps to open a demat account with Zerodha
 				</h2>
-				<div className="flex flex-col justify-around px-5 ">
-					<div className="">
+				<div className="holder flex flex-col md:flex-row justify-evenly items-center">
+					<div className="mx-auto md:m-0">
 						<img src={steps_acop} alt="Steps to open a demat account" />
 					</div>
-					<div className="text-[15px] mt-8">
-						<div className="flex flex-row justify-start items-center h-14 border-bottom">
+					<div className="text-[15px] md:text-xl mt-8">
+						<div className="flex flex-row justify-start items-center h-14 md:h-20 border-bottom">
 							<span className="block border border-zinc-300 rounded-full size-8 text-lg text-center">
 								01
 							</span>
-							<p className="m-4">Enter the requested details</p>
+							<p className="m-4 md:m-5">Enter the requested details</p>
 						</div>
-						<div className="flex flex-row justify-start items-center h-14 border-bottom">
+						<div className="flex flex-row justify-start items-center h-14 md:h-20 border-bottom">
 							<span className="block border border-zinc-300 rounded-full size-8 text-lg text-center">
 								02
 							</span>
-							<p className="m-4">Complete e-sign & verification</p>
+							<p className="m-4 md:m-5">Complete e-sign & verification</p>
 						</div>
-						<div className="flex flex-row justify-start items-center h-14 border-bottom">
+						<div className="flex flex-row justify-start items-center h-14 md:h-20 border-bottom">
 							<span className="block border border-zinc-300 rounded-full size-8 text-lg text-center">
 								03
 							</span>
-							<p className="m-4">Start investing!</p>
+							<p className="m-4 md:m-5">Start investing!</p>
 						</div>
 					</div>
 				</div>
