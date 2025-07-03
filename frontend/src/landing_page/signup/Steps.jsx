f29e9c7e8db01cf2c34309c2facdd138bf8d564e
VedantPatil-99 <vedantpatil9977@gmail.com>
2025-07-03 23:58:42 +0530

    feat: Implement Steps component for demat account opening process

--- frontend/src/landing_page/signup/Steps.jsx
+++ frontend/src/landing_page/signup/Steps.jsx
@@ -1,7 +1,34 @@
-import React from "react";
+import steps_acop from "@assets/images/steps-acop.svg";
 
 const Steps = () => {
-	return <div>Steps</div>;
+	return (
+		<>
+			<section className="section-spacing-reverse bg-zinc-50 mt-4 font-medium text-zinc-600">
+				<h2 className="text-xl mb-12 text-center  ">
+					Steps to open a demat account with Zerodha
+				</h2>
+				<div className="flex flex-col justify-around px-5 ">
+					<div className="">
+						<img src={steps_acop} alt="Steps to open a demat account" />
+					</div>
+					<div className="text-[15px]">
+						<div className="flex flex-row mt-8 justify-start items-center h-12 border-bottom">
+							<span className="border rounded-full size-6 text-lg text-center">
+								01
+							</span>
+							<p className="m-4">Enter the requested details</p>
+						</div>
+						<div className="flex flex-row">
+							<span>02</span> <p>Complete e-sign & verification</p>
+						</div>
+						<div className="flex flex-row">
+							<span>03</span> <p>Start investing!</p>
+						</div>
+					</div>
+				</div>
+			</section>
+		</>
+	);
 };
 
 export default Steps;
