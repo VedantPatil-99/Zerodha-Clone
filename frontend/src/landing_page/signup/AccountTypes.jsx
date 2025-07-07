fc60f5458a34fa5c4e52d75416c13d99a3d5ea4f
VedantPatil-99 <vedantpatil9977@gmail.com>
2025-07-07 19:43:23 +0530

    feat: Implement layout of AccountTypes component for account type exploration

--- frontend/src/landing_page/signup/AccountTypes.jsx
+++ frontend/src/landing_page/signup/AccountTypes.jsx
@@ -1,7 +1,16 @@
-import React from "react";
-
 const AccountTypes = () => {
-	return <div>AccountTypes</div>;
+	return (
+		<>
+			<section className="signup-spacing">
+				<div className="holder">
+					<h2 className="text-xl text-center text-zinc-700 font-medium mt-12">
+						Explore different account types
+					</h2>
+				</div>
+				<div className="flex flex-col justify-between"></div>
+			</section>
+		</>
+	);
 };
 
 export default AccountTypes;
