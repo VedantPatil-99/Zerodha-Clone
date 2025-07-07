0aa21587276882b8063ac51b3c4491f9f66000ea
VedantPatil-99 <vedantpatil9977@gmail.com>
2025-07-07 13:22:50 +0530

    feat: Refactor Benefits and BenefitFrag components for improved layout and styling

--- frontend/src/landing_page/signup/BenefitFrag.jsx
+++ frontend/src/landing_page/signup/BenefitFrag.jsx
@@ -6,7 +6,7 @@ const BenefitFrag = () => {
 			{signupPageData.zerodhaBenefits.map((bf, id) => (
 				<div key={id} className="text-zinc-600 leading-7">
 					<h3 className="text-xl font-medium mb-3">{bf.bfName}</h3>
-					<p className="text-[16px] text-zinc-500 my-4">{bf.bfDesc}</p>
+					<p className="text-[16.5px] text-zinc-500 my-4">{bf.bfDesc}</p>
 				</div>
 			))}
 		</>
--- frontend/src/landing_page/signup/Benefits.jsx
+++ frontend/src/landing_page/signup/Benefits.jsx
@@ -4,18 +4,18 @@ import BenefitFrag from "./BenefitFrag";
 const Benefits = () => {
 	return (
 		<>
-			<section className="signup-spacing">
-				<div className="flex-flex-col">
+			<section className="signup-spacing md:mt-10 flex flex-col md:flex-row md:items-center md:gap-8 mx-auto lg:justify-evenly">
+				<div className="max-w-lg">
 					<img
 						src={acop_benefits}
 						alt="benefits of Zerodha"
-						className="mx-auto my-5 w-full"
+						className="mx-auto my-4 lg:mb-6 w-full md:max-w-3xl lg:w-sm md:mx-0 lg:mx-auto"
 					/>
-					<h2 className="mb-12 text-xl font-medium text-zinc-600 leading-8">
+					<h2 className="mb-12 text-xl md:text-4xl lg:text-3xl font-medium text-zinc-700 leading-8 md:leading-14 lg:leading-12">
 						Benefits of opening a Zerodha demat account
 					</h2>
 				</div>
-				<div className="flex flex-col justify-between gap-6">
+				<div className="flex flex-col justify-between gap-6 max-w-lg">
 					<BenefitFrag />
 				</div>
 			</section>
