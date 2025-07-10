07e54a3c1a2c0a34a8bb660265de976c16c192ae
VedantPatil-99 <vedantpatil9977@gmail.com>
2025-07-11 01:13:34 +0530

    fix: Adjust heading size and spacing in AccountTypes component for improved layout

--- frontend/src/landing_page/signup/AccountTypes.jsx
+++ frontend/src/landing_page/signup/AccountTypes.jsx
@@ -5,11 +5,11 @@ const AccountTypes = () => {
 		<>
 			<section className="signup-spacing">
 				<div className="holder">
-					<h2 className="text-xl text-center text-zinc-700 font-medium mb-12">
+					<h2 className="text-xl md:text-4xl text-center text-zinc-700 font-medium mb-12">
 						Explore different account types
 					</h2>
 				</div>
-				<div className="holder">
+				<div className="holder md:py-5">
 					<AccTypeCard />
 				</div>
 			</section>
