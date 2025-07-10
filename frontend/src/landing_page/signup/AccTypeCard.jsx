356cd1e99742566d04729fe0120fddea418454f8
VedantPatil-99 <vedantpatil9977@gmail.com>
2025-07-11 00:25:43 +0530

    feat: Add image display to AccTypeCard for enhanced account type presentation

--- frontend/src/landing_page/signup/AccTypeCard.jsx
+++ frontend/src/landing_page/signup/AccTypeCard.jsx
@@ -11,6 +11,11 @@ const AccTypeCard = () => {
 					<h3 className="text-lg mb-5 text-zinc-700 font-medium">
 						{acc.title}
 					</h3>
+					<img
+						src={acc.img}
+						alt={`${acc.title.toLowerCase()} opening`}
+						className="absolute top-8 -left-4 size-8 bg-blue-50 p-1 rounded-full"
+					/>
 					<p className="text-[16.5px] mt-4 text-zinc-500 leading-7">
 						{acc.description}
 					</p>
