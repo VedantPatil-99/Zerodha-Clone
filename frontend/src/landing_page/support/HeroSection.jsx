792b9a20d7683d31b0b5922aec39f7b2b8010c85
VedantPatil-99 <vedantpatil9977@gmail.com>
2025-07-14 23:34:15 +0530

    feat: Layout HeroSection with support portal and ticket tracking links

--- frontend/src/landing_page/support/HeroSection.jsx
+++ frontend/src/landing_page/support/HeroSection.jsx
@@ -1,7 +1,18 @@
-import React from "react";
-
 const HeroSection = () => {
-	return <div>HeroSection</div>;
+	return (
+		<>
+			<section className="section-spacing h-screen md:h-96 bg-sky-600 text-white">
+				<div className="flex justify-between">
+					<a href="#" className="text-lg md:text-xl font-medium">
+						Support Portal
+					</a>
+					<a href="#" className="underline decoration-1 underline-offset-6">
+						Track tickets
+					</a>
+				</div>
+			</section>
+		</>
+	);
 };
 
 export default HeroSection;
