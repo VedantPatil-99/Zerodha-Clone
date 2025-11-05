fb61149b47eea0bdf32fcb98eef864ffd940d7dc
VedantPatil-99 <vedantpatil9977@gmail.com>
2025-11-05 23:07:34 +0530

    feat: Add navigation links to Menu component and manage active state

--- dashboard/src/components/Menu.jsx
+++ dashboard/src/components/Menu.jsx
@@ -1,6 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 
 const Menu = () => {
+	const [isOptActive, setIsOptActive] =
+		useState(0);
+	const [isOpen, setIsOpen] = useState(false);
+
+	const handleActiveOption = (index) => {
+		setIsOptActive(index);
+	};
+
+	const handleProfileOpen = (index) => {
+		setIsOpen(!isOpen);
+	};
+
+	const menu = "menu";
+	const activeMenu = "menu selected";
 	return (
 		<div className="menu-container">
 			<img
@@ -11,26 +26,112 @@ const Menu = () => {
 			<div className="menus">
 				<ul>
 					<li>
-						<p>Dashboard</p>
+						<Link
+							to="/"
+							style={{ textDecoration: "none" }}
+							onClick={() =>
+								handleActiveOption(0)
+							}>
+							<p
+								className={
+									isOptActive === 0
+										? activeMenu
+										: menu
+								}>
+								Dashboard
+							</p>
+						</Link>
 					</li>
 					<li>
-						<p>Orders</p>
+						<Link
+							to="/orders"
+							style={{ textDecoration: "none" }}
+							onClick={() =>
+								handleActiveOption(1)
+							}>
+							<p
+								className={
+									isOptActive === 1
+										? activeMenu
+										: menu
+								}>
+								Orders
+							</p>
+						</Link>
 					</li>
 					<li>
-						<p>Holdings</p>
+						<Link
+							to="/holdings"
+							style={{ textDecoration: "none" }}
+							onClick={() =>
+								handleActiveOption(2)
+							}>
+							<p
+								className={
+									isOptActive === 2
+										? activeMenu
+										: menu
+								}>
+								Holdings
+							</p>
+						</Link>
 					</li>
 					<li>
-						<p>Positions</p>
+						<Link
+							to="/positions"
+							style={{ textDecoration: "none" }}
+							onClick={() =>
+								handleActiveOption(3)
+							}>
+							<p
+								className={
+									isOptActive === 3
+										? activeMenu
+										: menu
+								}>
+								Positions
+							</p>
+						</Link>
 					</li>
 					<li>
-						<p>Funds</p>
+						<Link
+							to="/funds"
+							style={{ textDecoration: "none" }}
+							onClick={() =>
+								handleActiveOption(4)
+							}>
+							<p
+								className={
+									isOptActive === 4
+										? activeMenu
+										: menu
+								}>
+								Funds
+							</p>
+						</Link>
 					</li>
 					<li>
-						<p>Apps</p>
+						<Link
+							to="/apps"
+							style={{ textDecoration: "none" }}
+							onClick={() =>
+								handleActiveOption(5)
+							}>
+							<p
+								className={
+									isOptActive === 5
+										? activeMenu
+										: menu
+								}>
+								Apps
+							</p>
+						</Link>
 					</li>
 				</ul>
 				<hr />
-				<div className="profile">
+				<div
+					className="profile"
+					onClick={handleProfileOpen}>
 					<div className="avatar">ZU</div>
 					<p className="username">USERID</p>
 				</div>
