65ef7012052e07bb3634d1f948ee5a0866043f24
VedantPatil-99 <vedantpatil9977@gmail.com>
2025-11-06 22:55:28 +0530

    Update dependencies and enhance WatchList component with dynamic stock items
    - Updated @emotion/react and @emotion/styled to version 11.14.0 and 11.14.1 respectively.
    - Upgraded @mui/material to version 5.18.0.
    - Refactored WatchList component to include dynamic rendering of stock items from demo data.
    - Added WatchListItem component to display individual stock details with hover actions.

--- dashboard/src/components/WatchList.jsx
+++ dashboard/src/components/WatchList.jsx
@@ -1,22 +1,73 @@
-import React from "react";
+import React, { useState } from "react";
+import { watchlist } from "./../data/demoData";
+import KeyboardArrowUpIcon from "@mui/icons-material/KeyboardArrowUp";
+import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
 
 const WatchList = () => {
-  return (
-    <div className="watchlist-container">
-      <div className="search-container">
-        <input
-          type="text"
-          name="search"
-          id="search"
-          placeholder="Search eg:infy, bse, nifty fut weekly, gold mcx"
-          className="search"
-        />
-        <span className="counts"> 9 / 50</span>
-      </div>
+	return (
+		<div className="watchlist-container">
+			<div className="search-container">
+				<input
+					type="text"
+					name="search"
+					id="search"
+					placeholder="Search eg:infy, bse, nifty fut weekly, gold mcx"
+					className="search"
+				/>
+				<span className="counts"> 9 / 50</span>
+			</div>
 
-      <ul className="list"></ul>
-    </div>
-  );
+			<ul className="list">
+				{watchlist.map((stock, idx) => {
+					return (
+						<WatchListItem
+							stock={stock}
+							key={idx}
+						/>
+					);
+				})}
+			</ul>
+		</div>
+	);
 };
 
 export default WatchList;
+
+const WatchListItem = ({ stock }) => {
+	const [showActions, setShowActions] =
+		useState(false);
+	const handleOnMouseEnter = (e) => {
+		setShowActions(true);
+	};
+	const handleOnMouseLeave = (e) => {
+		setShowActions(false);
+	};
+
+	return (
+		<li
+			onMouseEnter={handleOnMouseEnter}
+			onMouseLeave={handleOnMouseLeave}>
+			<div className="item">
+				<p
+					className={
+						stock.isDown ? "down" : "up"
+					}>
+					{stock.name}
+				</p>
+				<div className="item-info">
+					<p className="percent">
+						{stock.percent}
+					</p>
+					<span>
+						{stock.isDown ? (
+							<KeyboardArrowDownIcon className="down" />
+						) : (
+							<KeyboardArrowUpIcon className="up" />
+						)}
+					</span>
+					<p className="price">{stock.price}</p>
+				</div>
+			</div>
+		</li>
+	);
+};
