0ca4030758954b7bbf6b0f23c74edb7437b475e7
VedantPatil-99 <vedantpatil9977@gmail.com>
2025-11-09 23:56:31 +0530

    Add get routes to get data of holdings and positions from db.

--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,30 @@
+import bodyParser from "body-parser";
+import cors from "cors";
 import "dotenv/config";
 import express from "express";
 import mongoose from "mongoose";
+import Holding from "./models/holding.js";
+import Position from "./models/position.js";
 
 const app = express();
 
 const PORT = process.env.PORT || 8081;
 const MONGO_URI = process.env.MONGO_URL;
 
-app.use(express.json());
+app.use(cors());
+app.use(bodyParser.json());
+
+app.get("/holdings", async (req, res) => {
+	const allHoldings = await Holding.find({});
+	// console.log(allHoldings);
+	res.json(allHoldings);
+});
+
+app.get("/positions", async (req, res) => {
+	const allPositions = await Position.find({});
+	// console.log(allPositions);
+	res.json(allPositions);
+});
 
 async function startServer() {
 	if (!MONGO_URI) {
